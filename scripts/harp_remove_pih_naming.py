0db53bd46e142314d8c1b8846d85239ed6ce02c7
Kat Seitz <katharina.seitz@northwestern.edu>
2022-10-12 15:34:17 -0500

    please work

--- scripts/harp_remove_pih_naming.py
+++ scripts/harp_remove_pih_naming.py
@@ -11,68 +11,51 @@ def rename_partic():
         parts = file.split(".")
         new_name = participant + "_ses-1_phase1." + parts[1]
         os.rename(file, new_name) 
-
     files = glob.glob(participant + "--FMAP2--GR--?_ph*")
     for file in files:
         parts = file.split(".")
         new_name = participant + "_ses-1_phase2." + parts[1]
         os.rename(file, new_name) 
-      
-
     files = glob.glob(participant + "--FMAP1--GR--*")
     for file in files:
         parts = file.split(".")
         new_name = participant + "_ses-1_magnitude1" + parts[1]
         os.rename(file, new_name)
-
-
     files = glob.glob(participant + "--FMAP2--GR--*")
     for file in files:
         parts = file.split(".")
         new_name = participant + "_ses-1_magnitude2" + parts[1]
         os.rename(file, new_name)
-
-
     files = glob.glob(participant + "--MID1--EP_RM--*")
     for file in files:
         parts = file.split(".")
         new_name = participant + "_ses-1_task-mid_run-01_bold" + parts[1]
         os.rename(file, new_name)
-
     files = glob.glob(participant + "--MID2--EP_RM--*")
     for file in files:
         parts = file.split(".")
         new_name = participant + "_ses-1_task-mid_run-02_bold" + parts[1]
         os.rename(file, new_name)
-
     files = glob.glob(participant + "REST1--EP_RM--*")
     for file in files:
         parts = file.split(".")
         new_name = participant + "_ses-1_task-rest_run-01_bold" + parts[1]
         os.rename(file, new_name)
-          
-
     files = glob.glob(participant + "--REST2--EP_RM--*")
     for file in files:
         parts = file.split(".")
         new_name = participant + "_ses-1_task-rest_run-02_bold" + parts[1]
         os.rename(file, new_name)
-          
-
     files = glob.glob(participant + "--REST3--EP_RM--11*")
     for file in files:
         parts = file.split(".")
         new_name = participant + "__ses-1_task-rest_run-03_bold" + parts[1]
         os.rename(file, new_name)
-
-
     files = glob.glob(participant + "--REST4--EP_RM--*")
     for file in files:
         parts = file.split(".")
         new_name = participant + "_ses-1_task-rest_run-04_bold" + parts[1]
         os.rename(file, new_name)
-
-
     files = glob.glob(participant + "--T1w--GR--*")
     for file in files:
         parts = file.split(".")
