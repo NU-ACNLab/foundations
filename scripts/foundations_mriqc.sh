22a755285bd781a2d0bdb0b19059173b942c124f
katseitz <katharinaseitz@WITIPRLW06KP212W2.local>
2022-12-09 12:51:02 -0600

    mriqc

---
+++ scripts/foundations_mriqc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#
+#SBATCH -A p31833
+#SBATCH -p short
+#SBATCH -t 04:00:00
+#SBATCH --mem=64G
+#SBATCH -J mriqc-hf
+#SBATCH	--mail-type=END,FAIL
+#SBATCH	--mail-user=katharinaseitz@northwestern.edu
+
+module purge
+module load singularity/latest
+echo "modules loaded"
+cd /projects/b1108
+
+singularity run --cleanenv -B /projects/b1108:/projects/b1108/ /projects/b1108/software/singularity_images/mriqc-latest.simg -v /projects/b1108/data/Georgia/foundations -v /projects/b1108/projects/georgia/foundations/mriqc participant --participant-label ${1}
+# change above to --clean-workdir so that the work directory is cleared and you save space
